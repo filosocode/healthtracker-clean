149d7907767106318be763fa08b339ce7bd1dbc4
91andres <91andresmunoz@gmail.com>
2025-06-28 05:12:58 -0500

    creacion demodelos minimos

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from patients.models import Patient
+
+
+class Report(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
